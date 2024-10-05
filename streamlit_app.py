36d4c3217571deeb0ace335fc2446990e529de1b
jtran6796 <jtran6796@gmail.com>
2024-10-05 12:43:12 -0400

    removed state related questions. (focusing only in florida)

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,42 +7,17 @@ st.title('Efficient Energy Calculator')
 with st.form("my_form"):
     dryer_stat = st.selectbox("How many times do you use your dryer a week?", ("1", "2", "3", "4+"))
 
-    sq_ft_stat = st.selectbox()
+    sq_ft_stat = st.selectbox("Total Square Footage", ("Less than 1,000", "1,000 - 1,499", "1,500 - 1,999", "2,000 - 2,499", "2,500 - 2,999", "3,000 or more"))
 
     fan_stat = st.selectbox("How many hours do you use your fan?", ("1", "2", "3", "4+"))
 
     fridge_stat = st.selectbox("How many fridges are in your household?", ("1", "2", "3+"))
 
-    state_stat = st.selectbox("What state do you live in?", ("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"))
-
-
     st.form_submit_button("Submit")
 
 
-state_dict = {
-    "northeast" : ["Maine", "New Hampshire", "Vermont", "Massachusetts", "Rhode Island", "Connecticut", "New York", "New Jersey", "Pennsylvania"],
-    "midwest" : ["Ohio", "Indiana", "Illinois", "Iowa", "Michigan", "Wisconsin", "Minnesota", "North Dakota", "South Dakota", "Nebraska", "Kansas", "Missouri"],
-    "south" : ["Delaware", "Maryland", "Virginia", "West Virginia", "North Carolina", "South Carolina", "Georgia", "Florida", "Alabama", "Mississippi", "Tennessee", "Kentucky", "Arkansas", "Louisiana", "Oklahoma", "Texas"],
-    "west" : ["Montana", "Idaho", "Wyoming", "Nevada", "Utah", "Colorado", "New Mexico", "Arizona", "Washington", "Oregon", "California", "Hawaii", "Alaska"]
-}
-
-region = state_dict.get(state_stat)
-
 DATE_COLUMN = 'date/time'
 DATA_URL = ""
-
-async def region_stat(dfs):
-    if region == "northeast":
-        DATA_URL = "https://www.eia.gov/consumption/residential/data/2020/c&e/xls/ce1.2.xlsx"
-    elif region == "midwest":
-        DATA_URL = "https://www.eia.gov/consumption/residential/data/2020/c&e/xls/ce1.3.xlsx"
-    elif region == "south":
-        DATA_URL = "https://www.eia.gov/consumption/residential/data/2020/c&e/xls/ce1.4.xlsx"
-    elif region == "west":
-        DATA_URL = "https://www.eia.gov/consumption/residential/data/2020/c&e/xls/ce1.5.xlsx"
-    await DATA_URL
-
-
 
     @st.cache_data
     def load_data(nrows):
