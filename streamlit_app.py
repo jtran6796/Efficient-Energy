577b145f5d3de9802b9268ea22a8aab5aa54b700
John Tran <jtran6796@gmail.com>
2024-10-05 12:59:40 -0400

    removed data loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,26 +16,6 @@ with st.form("my_form"):
     st.form_submit_button("Submit")
 
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ""
-
-    @st.cache_data
-    def load_data(nrows):
-        data = pd.read_excel(DATA_URL, nrows=nrows) #https://stackoverflow.com/questions/16888888/how-to-read-a-xlsx-file-using-the-pandas-library-in-ipython
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-    # Load 10,000 rows of data into the dataframe.
-    data = load_data(10000)
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text("Done! (using st.cache_data)")
-
-    st.subheader('Raw data')
-    st.map(data)
 
 
 
