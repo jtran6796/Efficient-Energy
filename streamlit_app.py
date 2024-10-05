2efc8482a25fa8b8f80a7db0007b164656ecd2c3
Mistlo <96539755+Ishankp@users.noreply.github.com>
2024-10-05 16:01:58 -0400

    Update streamlit_app.py
    added switch case for sqft and watt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
 import streamlit as st
+import streamlit_option_menu as option_menu 
 import pandas as pd
-import requests
 
 st.title('Efficient Energy Calculator')
 
-st.form("my_form"):
-
-    tot_kWh = st.number_input("Insert your monthly kW/h")
-
-    sq_ft_stat = st.selectbox("Total Square Footage", ("Less than 1,000", "1,000 - 1,499", "1,500 - 1,999", "2,000 - 2,499", "2,500 - 2,999", "3,000 or more"))
-
-    address = st.text_input("Insert your address")
-
-if(st.form_submit_button("Submit, "))
-
-#geocoding API
-#https://developers.google.com/maps/documentation/geocoding/requests-geocoding
-url = 'https://solar.googleapis.com/v1/dataLayers:get?'
-r_geo = requests.get(url + address)
+with st.form("my_form"):
 
 
-
-#solar API datalayers endpoint
-#https://developers.google.com/maps/documentation/solar/data-layers
-url = 'https://solar.googleapis.com/v1/dataLayers:get?'
-
-r = requests.get(url + r_geo )
+    sq_ft_stat = st.selectbox("Total Square Footage", ("Less than 1,000", "1,000 - 1,499", "1,500 - 1,999", "2,000 - 2,499", "2,500 - 2,999", "3,000 or more")
+    ,index = None,placeholder = "Please select an Option")
 
 
+    submitted = st.form_submit_button("Submit")
 
+    if submitted:
+        match sq_ft_stat:
+            case "Less than 1,000" : 
+                st.write("The average electricity usage for your home is 6,802 kWh")
+            case "1,000 - 1,499":
+                st.write("The average electricity usage for your home is 9,407 kWh")
+            case "1,500 - 1,999":
+                st.write("The average electricity usage for your home is 11,009 kWh")
+            case "2,000 - 2,499":
+                st.write("The average electricity usage for your home is 12,173 kWh")
+            case "2,500 - 2,999":
+                st.write("The average electricity usage for your home is 13,031 kWh")
+            case "3,000 or more":
+                st.write("The average electricity usage for your home is 15,489 kWh")
+            
 
-
-
-
-
-
-
+            case NONE:
+                st.write("Please Select An Option Before Submitting")
