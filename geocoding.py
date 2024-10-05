63cee9a4519b5212b8330692b3b57dcc74e1fd75
Raoul Nelson <nelson.raoula@gmail.com>
2024-10-05 18:05:45 -0400

    Add geocoding API to convert address to lat and long coordinates

---
+++ geocoding.py
@@ -0,0 +1,28 @@
+import googlemaps
+import array as arr
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+def get_coordinates(address):
+
+    # Load environment variables from .env file
+    load_dotenv(find_dotenv())
+
+    # Retrieve Google API KEY from env file
+    api_key = os.getenv('GOOGLE_KEY')
+
+    # Create a client object and pass API Key to it
+    gmaps = googlemaps.Client(key = api_key)
+
+    # Call geocode API, retrieve JSON response from address input
+    geocode_json = gmaps.geocode(address)
+
+    # Extract latitude and longitude from JSON result
+    lat_lon_json = geocode_json[0]['geometry']['location']
+    lat_lon_array = [lat_lon_json.get("lat"), lat_lon_json.get("lng")]
+
+    # Return lat and lon
+    return lat_lon_array
+
+get_coordinates('7822 Elmstone Circle, Orlando, FL')
