f6d915f7cf0e06458e9970e97829bc39bfa2b3d5
Lou Vaughn <ljvaughn0841>
2024-10-05 16:42:54 -0400

    add solar api request

--- solar.py
+++ solar.py
@@ -1,11 +1,21 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
-def solar_estimation():
+def solar_estimation(lat_long):
 
-    lat_long = json.dumps({
-        "latitude": 28.603330,
-        "longitude": -81.239190
-    })
+    load_dotenv()
 
-    response = requests.get("https://solar.googleapis.com/v1/dataLayers:get?" + )
+    APIKEY = os.getenv("GOOGLEKEY")
+
+    response = requests.get("https://solar.googleapis.com/v1/buildingInsights:findClosest?"+
+                            "location.latitude=" + str(lat_long[0]) +
+                            "&location.longitude="+ str(lat_long[1])+
+                            "&requiredQuality=HIGH"
+                            "&key=" + APIKEY)
+    
+
+    print(response)
+
+solar_estimation([37.4450, -122.1390])
